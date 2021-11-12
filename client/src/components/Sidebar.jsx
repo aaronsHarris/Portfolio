45bcbdb0d6898cfc8c919a6c165189d6c38688ae
Aaron Harris <aaronsharris@gmail.com>
2021-11-11 19:56:40 -0600

    added styles to sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -10,15 +10,16 @@ import { NavLink } from "react-router-dom";
 const Container = styled.div`
   position: fixed;
   .active {
-    border-right: 4px solid var(--white);
+    border-right: 4px solid var(--orange);
     img {
       filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg)
         brightness(103%) contrast(103%);
     }
   }
 `;
+
 const Button = styled.button`
-  background-color: var(--black);
+  background-color: var(--blue);
   border: none;
   width: 2.5rem;
   height: 2.5rem;
@@ -49,7 +50,7 @@ const Button = styled.button`
 `;
 
 const SidebarContainer = styled.div`
-  background-color: var(--black);
+  background-color: var(--blue);
   width: 3.5rem;
   height: 80vh;
   margin-top: 1rem;
@@ -76,15 +77,113 @@ const SlickBar = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: var(--black);
+  background-color: var(--blue);
   padding: 2rem 0;
-  
   position: absolute;
   top: 6rem;
   left: 0;
   width: ${(props) => (props.clicked ? "12rem" : "3.5rem")};
   transition: all 0.5s ease;
   border-radius: 0 30px 30px 0;
+`;
+
+const Item = styled(NavLink)`
+  text-decoration: none;
+  color: var(--white);
+  width: 100%;
+  padding: 1rem 0;
+  cursor: pointer;
+  display: flex;
+  padding-left: 1rem;
+  &:hover {
+    border-right: 4px solid var(--orange);
+    img {
+      filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg)
+        brightness(103%) contrast(103%);
+    }
+  }
+  img {
+    width: 1.2rem;
+    height: auto;
+    filter: invert(92%) sepia(4%) saturate(1033%) hue-rotate(169deg)
+      brightness(78%) contrast(85%);
+  }
+`;
+
+const Text = styled.span`
+  width: ${(props) => (props.clicked ? "100%" : "0")};
+  overflow: hidden;
+  margin-left: ${(props) => (props.clicked ? "1.5rem" : "0")};
+  transition: all 0.3s ease;
+`;
+
+const Profile = styled.div`
+  width: ${(props) => (props.clicked ? "14rem" : "3rem")};
+  height: 3rem;
+  padding: 0.5rem 1rem;
+  /* border: 2px solid var(--orange); */
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: ${(props) => (props.clicked ? "11rem" : "0")};
+  background-color: var(--blue);
+  color: var(--orange);
+  transition: all 0.3s ease;
+  img {
+    width: 2.5rem;
+    height: 2.5rem;
+    border-radius: 50%;
+    cursor: pointer;
+    &:hover {
+      border: 2px solid var(--white);
+      padding: 2px;
+    }
+  }
+`;
+
+const Details = styled.div`
+  display: ${(props) => (props.clicked ? "flex" : "none")};
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Name = styled.div`
+  padding: 0 1.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h4 {
+    display: inline-block;
+  }
+  a {
+    font-size: 0.8rem;
+    text-decoration: none;
+    color: var(--white);
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const Logout = styled.button`
+  border: none;
+  width: 2rem;
+  height: 2rem;
+  background-color: transparent;
+  img {
+    width: 100%;
+    height: auto;
+    filter: invert(15%) sepia(70%) saturate(6573%) hue-rotate(2deg)
+      brightness(100%) contrast(126%);
+    transition: all 0.3s ease;
+    &:hover {
+      border: none;
+      padding: 0;
+      opacity: 0.5;
+    }
+  }
 `;
 
 
@@ -96,53 +195,77 @@ const Sidebar = () => {
   const [profileClick, setprofileClick] = useState(false);
   const handleProfileClick = () => setprofileClick(!profileClick);
   return (
-    <>
-      <Button clicked={click} onClick={() => handleClick()}>
-        Click
-      </Button>
-      <SidebarContainer>
-        <div>
-          <Logo>
-            <img src={logo} alt="logo" />
-          </Logo>
-          <SlickBar clicked={click}>
-            <li>
-              <img src={Home} alt="Home" />
-              <span>Home</span>
-            </li>
-            <li>
-              <img src={star} alt="Star" />
-              <span>About</span>
-            </li>
-            <li>
-              <img src={folder} alt="projects" />
-              <span>projects</span>
-            </li>
-            <li>
-              <img src={contact} alt="contact" />
-              <span>contact</span>
-            </li>
-            <li>
-              <img src={star} alt="star" />
-              <span>star</span>
-            </li>
-          <div>
-            <img src="profile" alt="profile" />
-            <div>
-              <div>
-                <h4>Aaron Harris</h4>
-                <a href="#">view profile</a>
-              </div>
-              <button>
-                <img src={star} alt="logout" />
-              </button>
-            </div>
-          </div>
-          </SlickBar>
-        </div>
-      </SidebarContainer>
-    </>
-  );
+    <Container>
+    <Button clicked={click} onClick={() => handleClick()}></Button>
+    <SidebarContainer>
+      {/* <Logo>
+        <img src={logo} alt="logo" />
+      </Logo> */}
+      <SlickBar clicked={click}>
+        <Item
+          onClick={() => setClick(false)}
+          exact
+          activeClassName="active"
+          to="/"
+        >
+          <img src={Home} alt="Home" />
+          <Text clicked={click}>Home</Text>
+        </Item>
+        <Item
+          onClick={() => setClick(false)}
+          activeClassName="active"
+          to="/about"
+        >
+          <img src={star} alt="about" />
+          <Text clicked={click}>About Me</Text>
+        </Item>
+        <Item
+          onClick={() => setClick(false)}
+          activeClassName="active"
+          to="/projects"
+        >
+          <img src={folder} alt="projects" />
+          <Text clicked={click}>Projects</Text>
+        </Item>
+        <Item
+          onClick={() => setClick(false)}
+          activeClassName="active"
+          to="/contact"
+        >
+          <img src={contact} alt="Contact" />
+          <Text clicked={click}>Contact</Text>
+        </Item>
+        {/* <Item
+          onClick={() => setClick(false)}
+          activeClassName="active"
+          to="/projects"
+        >
+          <img src={folder} alt="Projects" />
+          <Text clicked={click}>Projects</Text>
+        </Item> */}
+      </SlickBar>
+
+      <Profile clicked={profileClick}>
+        <img
+          onClick={() => handleProfileClick()}
+          src="https://picsum.photos/200"
+          alt="Profile"
+        />
+        <Details clicked={profileClick}>
+          <Name>
+            <h4>Aaron&nbsp;Harris</h4>
+            <a href="#">view&nbsp;GitHub</a>
+          </Name>
+
+          {/* <Logout>
+            <img src={star} alt="logout" />
+          </Logout> */}
+        </Details>
+      </Profile>
+    </SidebarContainer>
+  </Container>
+);
 };
+ 
 
 export default Sidebar;
